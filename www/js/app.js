80731a4e6b369f7c96204e58a00eb6e47174f18d
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-30 20:16:04 +0200

    Fixe bug Cross Domain

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,11 @@
 
 var app = angular.module('app', ['ngRoute']);
 
-app.config(function($routeProvider) {
+app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
     $routeProvider
         .when('/home', { templateUrl: 'partials/home.html' })
         .when('/about', { templateUrl: 'partials/about.html' })
         .otherwise({ redirectTo: '/home' });
-});
+}]);
--- www/js/controller/WeatherCtrl.js
+++ www/js/controller/WeatherCtrl.js
@@ -1,11 +1,18 @@
-app.controller('WeatherCtrl', function($scope) {
-
-    $scope.city = "Montpellier"
-
-    var time = "36.29"
+app.controller('WeatherCtrl', function($scope, $http) {
 
     $scope.search = function() {
+        var appid = "555f2a1d504b2f6eeae38744267aef59";
+        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + $scope.city + "&appid=" + appid + "&units=metric";
+        console.log(url);
+        $http.get(url).success(httpSuccess).error(function() {
+            alert("Impossible de récupérer l'information");
+        });
+    }
 
+    httpSuccess = function(res) {
+        alert(1);
+        console.log(res.weather);
+        $scope.weather = res;
     }
 
 });
