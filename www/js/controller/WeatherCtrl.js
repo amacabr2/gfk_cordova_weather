209d980f12a63e594e57b575e51e01d82f2fbeb4
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-31 18:00:31 +0200

    Le loader s'affiche lors de la recherche puis s'enlève

--- www/js/controller/WeatherCtrl.js
+++ www/js/controller/WeatherCtrl.js
@@ -1,16 +1,19 @@
 app.controller('WeatherCtrl', function($scope, $http) {
 
+    $scope.loader = false;
+
     $scope.search = function() {
         var appid = "555f2a1d504b2f6eeae38744267aef59";
         var url = "http://api.openweathermap.org/data/2.5/weather?q=" + $scope.city + "&appid=" + appid + "&units=metric";
-        console.log(url);
+        $scope.loader = true;
         $http.get(url).success(httpSuccess).error(function() {
+            $scope.loader = false;
             alert("Impossible de récupérer l'information");
         });
     }
 
     httpSuccess = function(res) {
-        var time = "40.15"
+        $scope.loader = false
         $scope.weather = res;
     }
 
