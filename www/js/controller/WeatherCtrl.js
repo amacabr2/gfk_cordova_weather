0cf158cc5507a50146eb3e6391758420b47aa4f3
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-05-31 18:19:15 +0200

    Affichage des températures

--- www/js/controller/WeatherCtrl.js
+++ www/js/controller/WeatherCtrl.js
@@ -4,7 +4,7 @@ app.controller('WeatherCtrl', function($scope, $http) {
 
     $scope.search = function() {
         var appid = "555f2a1d504b2f6eeae38744267aef59";
-        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + $scope.city + "&appid=" + appid + "&units=metric";
+        var url = "http://api.openweathermap.org/data/2.5/forecast?q=" + $scope.city + "&appid=" + appid + "&units=metric";
         $scope.loader = true;
         $http.get(url).success(httpSuccess).error(function() {
             $scope.loader = false;
