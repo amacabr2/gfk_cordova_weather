252abb1f5368189115794d8993abf3d00188cc69
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-02 19:56:04 +0200

    Design pour les info supplémentaires lorsqu'on clique sur une date

--- www/js/controller/WeatherCtrl.js
+++ www/js/controller/WeatherCtrl.js
@@ -3,6 +3,7 @@ app.controller('WeatherCtrl', function($scope, $http) {
     $scope.Math = Math;
     $scope.loader = false;
     $scope.found = false;
+    $scope.id = 0;
 
     $scope.search = function() {
         var appid = "555f2a1d504b2f6eeae38744267aef59";
@@ -12,6 +13,16 @@ app.controller('WeatherCtrl', function($scope, $http) {
             $scope.loader = false;
             alert("Impossible de récupérer l'information");
         });
+    }
+
+    $scope.expand = function(e) {
+        $elem = $(e.currentTarget);
+        $elem.addClass('row-active').siblings().removeClass('row-active');
+        $scope.id = $elem[0].id;
+    }
+
+    $scope.expanded = function(id) {
+        return id == $scope.id;
     }
 
     httpSuccess = function(res) {
