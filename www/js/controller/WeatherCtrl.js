dd9bd4b5213a03d9790fd206c88e0d2498a543a7
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-06-03 10:24:05 +0200

    Geolocalisation

--- www/js/controller/WeatherCtrl.js
+++ www/js/controller/WeatherCtrl.js
@@ -1,18 +1,15 @@
-app.controller('WeatherCtrl', function($scope, $http) {
+app.controller('WeatherCtrl', function($scope, $http, GeolocationService) {
 
     $scope.Math = Math;
     $scope.loader = false;
     $scope.panel = 0;
     $scope.id = 0;
+    $scope.appid = "555f2a1d504b2f6eeae38744267aef59";
 
     $scope.search = function() {
-        var appid = "555f2a1d504b2f6eeae38744267aef59";
-        var url = "http://api.openweathermap.org/data/2.5/forecast?q=" + $scope.city + "&appid=" + appid + "&units=metric";
+        var url = "http://api.openweathermap.org/data/2.5/forecast?q=" + $scope.city + "&appid=" + $scope.appid + "&units=metric";
         $scope.loader = true;
-        $http.get(url).success(httpSuccess).error(function() {
-            $scope.loader = false;
-            alert("Impossible de récupérer l'information");
-        });
+        $http.get(url).success(httpSuccess).error(httpError);
     }
 
     $scope.expand = function(e) {
@@ -26,13 +23,22 @@ app.controller('WeatherCtrl', function($scope, $http) {
     }
 
     $scope.geolocate = function() {
-
+        $scope.loader = true;
+        GeolocationService.getCurrentPosition(function(position) {
+            var url = "http://api.openweathermap.org/data/2.5/forecast?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=" + $scope.appid + "&units=metric&cnt=10";
+            $http.get(url).success(httpSuccess).error(httpError);
+        });
     }
 
     httpSuccess = function(res) {
         $scope.panel = 1;
         $scope.loader = false;
         $scope.weather = res;
+    }
+
+    httpError = function(res) {
+        $scope.loader = false;
+        alert("Impossible de récupérer l'information");
     }
 
 });
